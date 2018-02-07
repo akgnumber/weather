96e5536fea9562a99a826bf5da1ef2fb66bcebd3
hsq <1271559307@qq.com>
2018-02-07 16:48:18 +0800

    2018-2-7

---
+++ micro-weather-report/src/main/java/com/waylau/spring/cloud/weather/job/WeatherDataSyncJob.java
@@ -0,0 +1,53 @@
+package com.waylau.spring.cloud.weather.job;
+
+import java.util.List;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import com.waylau.spring.cloud.weather.service.CityDataService;
+import com.waylau.spring.cloud.weather.service.WeatherDataService;
+import com.waylau.spring.cloud.weather.vo.City;
+
+/**
+ * 
+ * @author Administrator
+ */
+public class WeatherDataSyncJob extends QuartzJobBean{
+	private final static Logger logger = LoggerFactory.getLogger(WeatherDataSyncJob.class);
+	
+	@Autowired
+	private CityDataService cityDataService;
+	@Autowired
+	private WeatherDataService weatherDataService;
+	
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		logger.info("Weather Data Sync Job. Start!");
+		//获取城市ID列表
+		List<City> cityList = null;
+		try {
+			cityList = cityDataService.listCity();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			logger.error("【从xml中获取城市列表ID异常】message={}",e);
+		}
+		//遍历城市ID获取天气
+		if (null == cityList) {
+			return;
+		}
+		for (City city:cityList) {
+			String cityId = city.getCityId();
+			logger.info("Weather Data Sync Job");
+			
+			weatherDataService.syncDataByCityId(cityId);
+		}
+		
+		logger.info("Weather Data Sync Job. End!");
+	}
+
+}
